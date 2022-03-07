87c0b80aa945ba07a61acf099b7de43b52be71c4
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-07 10:38:51 +0900

    [Update] - Updated Add.ts and Index.ts file

--- 04/04-4/src/index.ts
+++ 04/04-4/src/index.ts
@@ -1,6 +1,7 @@
 import * as callback from "./utils/callback";
 import { calc } from "./utils/nested";
 import { result } from "./utils/high-order";
+import { add } from "./utils/add";
 
 console.log("\nCall Back Result -> ", callback);
 console.log("\n-----------------------\n");
@@ -9,3 +10,6 @@ calc(30, (result: number) => console.log(`Calc Result is --> ${result}`));
 console.log("\n-----------------------\n");
 
 console.log("Result -> ", result);
+console.log("\n-----------------------\n");
+
+console.log("Add -> ", add(1));
