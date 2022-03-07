8e5781b2d72cf7efc48cdbedc87811335bdce90b
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-07 16:56:14 +0900

    [Feature] - Index.ts and Array-type has been updated

--- 05/05-1/src/index.ts
+++ 05/05-1/src/index.ts
@@ -1,7 +1,11 @@
 import { numbers, strings } from "./utils/array";
+import { IPerson, numArray, personArray, strArray } from "./utils/array-type";
 import { a, o } from "./utils/isArray";
 
 console.log("\nNumbers & Strings -> ", numbers, strings);
 console.log("Check is Array -> ", Array.isArray(a), Array.isArray(o))
 console.log("------------------------------------");
 
+console.log("Number Array -> ", numArray);
+console.log("String Array -> ", strArray);
+console.log("IPerson Array -> ", personArray);
---
+++ 05/05-1/src/utils/array-type.ts
@@ -0,0 +1,5 @@
+export let numArray: number[] = [1, 2, 3]
+export let strArray: string[] = ['Hello', 'World']
+
+export type IPerson = {name: string, age?: number}
+export let personArray: IPerson[] = [{name: 'Jack'}, {name: 'Jane', age: 32}]
