6a3862d087919c71000e9e4902a5e24520e45319
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-07 16:48:03 +0900

    [Feature] - Added IsArray.ts file

--- 05/05-1/src/index.ts
+++ 05/05-1/src/index.ts
@@ -1,3 +1,7 @@
 import { numbers, strings } from "./utils/array";
+import { a, o } from "./utils/isArray";
 
-console.log(numbers, strings);
+console.log("\nNumbers & Strings -> ", numbers, strings);
+console.log("Check is Array -> ", Array.isArray(a), Array.isArray(o))
+console.log("------------------------------------");
+
---
+++ 05/05-1/src/utils/isArray.ts
@@ -0,0 +1,2 @@
+export let a = [1, 2, 3]
+export let o = {name: 'Jack', age: 32}
