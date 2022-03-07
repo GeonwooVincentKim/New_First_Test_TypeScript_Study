84fed0885c7a5c61738930ab1a1df85bfdb161cf
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-07 13:21:27 +0900

    [Update] - Updated Default.ts and Index.ts file

--- 04/04-5/src/index.ts
+++ 04/04-5/src/index.ts
@@ -1,4 +1,7 @@
-import { makePerson } from "./utils/default";
+import { makePerson, makePerson2, printPerson } from "./utils/default";
 
 console.log("Make Person (Name) -> ", makePerson('Jack'));
 console.log("Make Person (Name)(Age) -> ", makePerson('Jane', 33));
+console.log("(Arrow Function - Return Value)Make Person 2 (Name)(Age) -> ", makePerson2('Jane', 33));
+
+console.log("Print Person -> ", printPerson({name: 'Jack', age: 10}));
--- 04/04-5/src/utils/default.ts
+++ 04/04-5/src/utils/default.ts
@@ -4,4 +4,9 @@ export const makePerson = (name: string, age: number = 10): Person => {
     const person = {name: name, age: age}
     return person
 }
+export const makePerson2 = (name: string, age: number = 10): Person => ({name, age})
 
+export const printPerson = ({name, age}: Person): void =>
+    console.log(`\nPrint Person -> Name: ${name}, age: ${age}`);
+
+printPerson({name: 'Jack', age: 10});
