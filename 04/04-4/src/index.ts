384f8b72e9af82e357f11a748ea465520d81e396
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-07 10:18:13 +0900

    [Feature] - Calc file has been updated

--- 04/04-4/src/index.ts
+++ 04/04-4/src/index.ts
@@ -1,3 +1,7 @@
 import * as callback from "./utils/callback";
+import { calc } from "./utils/nested";
+
 console.log("\nCall Back Result -> ", callback);
 console.log("\n-----------------------\n");
+
+calc(30, (result: number) => console.log(`Calc Result is --> ${result}`)); 
