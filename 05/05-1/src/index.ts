4b502cc5deee0ffbe6a79c40bf13b6af8404a040
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-08 10:25:31 +0900

    [Feature] - Split.ts file has been created

--- 05/05-1/src/index.ts
+++ 05/05-1/src/index.ts
@@ -1,6 +1,7 @@
 import { numbers, strings } from "./utils/array";
 import { IPerson, numArray, personArray, strArray } from "./utils/array-type";
 import { a, o } from "./utils/isArray";
+import { split } from "./utils/split";
 
 console.log("\nNumbers & Strings -> ", numbers, strings);
 console.log("Check is Array -> ", Array.isArray(a), Array.isArray(o))
@@ -9,3 +10,8 @@ console.log("------------------------------------");
 console.log("Number Array -> ", numArray);
 console.log("String Array -> ", strArray);
 console.log("IPerson Array -> ", personArray);
+console.log("------------------------------------");
+
+console.log("Split -> ", split('Hello'), split('H_e_l_l_o', '_'))
+console.log("------------------------------------");
+
