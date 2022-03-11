dbfd3d4bce0a98ea547e637fcdc8b13a82477631
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-11 14:33:35 +0900

    [Feature] - Declarative Sum

--- 05/05-2/src/index.ts
+++ 05/05-2/src/index.ts
@@ -1,4 +1,10 @@
+import { numbers } from "./utils/declarative-sum";
 import { sum } from "./utils/imperative-sum";
 
 console.log("\n-----------------------");
 console.log("Imperative Sum -> ", sum);
+
+console.log("-----------------------");
+console.log("Declarative Sum -> ", numbers);
+
+console.log("-----------------------\n");
--- 05/05-2/src/utils/declarative-sum.ts
+++ 05/05-2/src/utils/declarative-sum.ts
@@ -1,2 +1,3 @@
 import { range } from "../../../05-1/src/utils/range";
 
+export let numbers: number[] = range(1, 100 + 1)
