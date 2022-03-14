0f79f6a87040acaa6f3ed4be214f34f8e89b2cb2
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-14 09:39:31 +0900

    [Update] - Updated array method chain.ts

---
+++ 05/05-3/src/index.ts
@@ -0,0 +1,6 @@
+import { chainResult } from "./utils/arrayMethodChain";
+
+console.log("\n------------------------------");
+
+console.log("Chain Result -> ", chainResult);
+console.log("------------------------------\n");
