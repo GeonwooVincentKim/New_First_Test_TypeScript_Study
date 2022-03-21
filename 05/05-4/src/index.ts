df86d15bff9af867819072846a1839be70605654
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-21 16:50:06 +0900

    [Feature] - Deep Copy and Shallow Copy

--- 05/05-4/src/index.ts
+++ 05/05-4/src/index.ts
@@ -1,6 +1,10 @@
 import { original, copied } from "./utils/deepCopy";
+import { originalArray, shallowCopiedArray } from "./utils/shallow-copy";
 
 console.log("\n------------------------");
 console.log("Deep Copy -> ", original, copied);
+
+console.log("------------------------");
+console.log("Shallow Copy -> ", originalArray, shallowCopiedArray);
 
 console.log("------------------------\n");
---
+++ 05/05-4/src/utils/shallow-copy.ts
@@ -0,0 +1,4 @@
+export const originalArray = [5, 3, 9, 7]
+export const shallowCopiedArray = originalArray
+
+shallowCopiedArray[0] = 0
