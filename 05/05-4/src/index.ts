01e55390d57220495032e4501285a2bd0f347304
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-21 17:10:03 +0900

    [Feature] - Deep Copy By Spread Operator

--- 05/05-4/src/index.ts
+++ 05/05-4/src/index.ts
@@ -1,4 +1,5 @@
 import { original, copied } from "./utils/deepCopy";
+import { deepCopiedArray, oArray } from "./utils/DeepCopyBySpreadOperator";
 import { originalArray, shallowCopiedArray } from "./utils/shallow-copy";
 
 console.log("\n------------------------");
@@ -6,5 +7,8 @@ console.log("Deep Copy -> ", original, copied);
 
 console.log("------------------------");
 console.log("Shallow Copy -> ", originalArray, shallowCopiedArray);
+
+console.log("------------------------");
+console.log("Deep Copy by Spread Operator -> ", oArray, deepCopiedArray);
 
 console.log("------------------------\n");
---
+++ 05/05-4/src/utils/DeepCopyBySpreadOperator.ts
@@ -0,0 +1,4 @@
+export const oArray = [1, 2, 3, 4]
+export const deepCopiedArray = [...oArray]
+
+deepCopiedArray[0] = 0
